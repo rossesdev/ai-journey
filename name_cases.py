0491dcd43cbe7032a5910a7c8fddb504bec68fd6
Rose <rosa0504m@outlook.com>
2025-06-25 11:00:25 -0500

    Differents activities

---
+++ name_cases.py
@@ -0,0 +1,26 @@
+# 2 - 3
+
+name = "Saray"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+# 2 - 4
+
+name_1 = "Carlos"
+print("Different ways", name_1.lower(), name_1.upper(), name_1.title())
+
+# 2 - 5 
+
+quote = 'Walt Disney once said, "If you can dream it, you can do it"'
+print(quote)
+
+# 2 - 6
+
+famous_person = "Walt Disney"
+message = "If you can dream it, you can do it"
+print(f'{famous_person} once said, "{message}"')
+
+# 2 - 7 
+
+name = "\tEsteban\t\n"
+print(name)
+print('rstrip',name.rstrip(), 'lstrip', name.lstrip(), 'strip', name.strip())
---
+++ symple_message.py
@@ -0,0 +1,4 @@
+message = "Hi, this is a different message"
+print(message)
+message = "It's rainning cats and dosgs"
+print(message)
